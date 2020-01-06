fee05b1ad7c3fe6978cca56ff7e117475f72c19c
QDSG <piftao1127@163.com>
2020-01-06 10:38:21 +0800

    $route

--- web_vue/vue_basic/11.vue_router/learn-vue-router/src/components/User.vue
+++ web_vue/vue_basic/11.vue_router/learn-vue-router/src/components/User.vue
@@ -2,10 +2,19 @@
   <div>
     <h2>我是用户页面</h2>
     <p>我是用户相关信息，嘻嘻嘻嘻</p>
+    <h3>{{userId}}</h3>
   </div>
 </template>
 
 <script>
+  export default {
+    name: 'User',
+    computed: {
+      userId() {
+        return this.$route.params.userId
+      }
+    }
+  }
 </script>
 
 <style>
