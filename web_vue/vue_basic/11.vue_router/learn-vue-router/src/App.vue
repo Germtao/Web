2181e3c98e78482ec5687a4402384d47fba390fa
QDSG <piftao1127@163.com>
2020-01-06 14:30:06 +0800

    参数传递-2

--- web_vue/vue_basic/11.vue_router/learn-vue-router/src/App.vue
+++ web_vue/vue_basic/11.vue_router/learn-vue-router/src/App.vue
@@ -2,10 +2,13 @@
   <div id="app">
     <router-link to="/home" tag="button" replace="">首页</router-link>
     <router-link to="/about" tag="button" replace="">关于</router-link>
-    <router-link :to="'/user/'+userId" tag="button" replace="">用户</router-link>
-    <router-link :to="{path: '/profile', query: {name: 'Kobe', age: 40, height: 1.98}}" tag="button" replace="">档案</router-link>
     <!-- <button @click="homeClick">首页</button> -->
     <!-- <button @click="aboutClick">关于</button> -->
+
+    <router-link :to="'/user/'+userId" tag="button" replace="">用户</router-link>
+    <!-- <router-link :to="{path: '/profile', query: {name: 'Kobe', age: 40, height: 1.98}}" tag="button" replace="">档案</router-link> -->
+    <button @click="profileClick">档案</button>
+
     <router-view/>
   </div>
 </template>
@@ -17,17 +20,28 @@ export default {
     return {
       userId: 'zhangsan'
     }
+  },
+  methods: {
+    // homeClick() {
+    //   // this.$router.push('/home')
+    //   this.$router.replace('/home')
+    // },
+    // aboutClick() {
+    //   // this.$router.push('/about')
+    //   this.$router.replace('/about')
+    // }
+
+    profileClick() {
+      this.$router.replace({
+        path: '/profile',
+        query: {
+          name: 'kobe',
+          age: 40,
+          height: 1.98
+        }
+      })
+    }
   }
-  // methods: {
-  //   homeClick() {
-  //     // this.$router.push('/home')
-  //     this.$router.replace('/home')
-  //   },
-  //   aboutClick() {
-  //     // this.$router.push('/about')
-  //     this.$router.replace('/about')
-  //   }
-  // }
 }
 </script>
 
