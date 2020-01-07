01e946b715c7df3eeefa1b439048be10c7a028d0
QDSG <piftao1127@163.com>
2020-01-07 18:22:32 +0800

    tabbar组件封装

--- web_vue/vue_basic/11.vue_router/tabbar/src/App.vue
+++ web_vue/vue_basic/11.vue_router/tabbar/src/App.vue
@@ -1,36 +1,21 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <router-view></router-view>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import MainTabBar from "./components/MainTabBar.vue"
+
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
+  }
 </script>
 
 <style>
   @import './assets/css/base.css';
-
-  #tab-bar {
-    display: flex;
-    background-color: #f6f6f6;
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    box-shadow: 0 -3px 1px rgba(100, 100, 100, .08);
-  }
-
-  .tab-bar-item {
-    flex: 1;
-    text-align: center;
-    height: 49px;
-  }
 </style>
--- web_vue/vue_basic/11.vue_router/tabbar/src/router/index.js
+++ web_vue/vue_basic/11.vue_router/tabbar/src/router/index.js
@@ -1,9 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Home = () => import('../pages/home/Home')
+const Category = () => import('../pages/category/Category')
+const Cart = () => import('../pages/cart/Cart')
+const Profile = () => import('../pages/profile/Profile')
+
+// 1. 安装插件
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-  ]
+// 2.创建路由对象
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home,
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new Router({
+  routes,
+  mode: 'history'
 })
+
+// 3. 导出router
+export default router
